8f9109ab193664505a95961904b0d42113fa76ae
rusty0515 <c20222799.decolongon@csav.edu.ph>
2025-01-25 12:46:24 +0800

    Erase

--- routes/web.php
+++ routes/web.php
@@ -7,11 +7,3 @@ Route::get('/', function () {
 });
 
  
-route::get('/dashboard', function () {
-    return view('dashboard');
-});
-
-Route::get('/login', function () {
-    return view('login');
-});
-//sfsfskfjskfskfhshfsh
